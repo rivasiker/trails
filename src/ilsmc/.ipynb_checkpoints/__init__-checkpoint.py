f5ef50595a618b4b67ff0d2904d6c84e69156ad8
rivasiker <ikerrivas96@gmail.com>
2022-04-05 17:05:57 +0200

    Update files

--- src/ilsmc/.ipynb_checkpoints/__init__-checkpoint.py
+++ src/ilsmc/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,3 +1,9 @@
-[build-system]
-requires = ["setuptools>=42"]
-build-backend = "setuptools.build_meta"
+import sys
+import numpy as np
+import pandas as pd
+from scipy.linalg import expm
+from scipy.stats import truncexpon
+from scipy.stats import expon
+from scipy.special import comb
+import ast
+import multiprocessing as mp
