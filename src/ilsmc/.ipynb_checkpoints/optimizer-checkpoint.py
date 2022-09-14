c2999d9a6fbbcf07a84cb636bb3bee9b0fcf7d4a
rivasiker <ikerrivas96@gmail.com>
2022-09-14 12:06:16 -0400

    Change parameterization of the optimizer

--- src/ilsmc/.ipynb_checkpoints/optimizer-checkpoint.py
+++ src/ilsmc/.ipynb_checkpoints/optimizer-checkpoint.py
@@ -22,22 +22,22 @@ def write_list(lst):
                 f.write(',')
         f.write('\n')
 
-def trans_emiss_calc(t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC):
+def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC):
     N_ref = N_AB
     t_A = t_1
     t_B = t_1
     t_AB = t_2
     t_C = t_1+t_2
     t_peak = 2*(N_ABC/N_ref)
-    rho_A = 2*N_A*r
-    rho_B = 2*N_B*r
+    rho_A = 2*N_AB*r
+    rho_B = 2*N_AB*r
     rho_AB = 2*N_AB*r
-    rho_C = 2*N_C*r
+    rho_C = 2*N_AB*r
     rho_ABC = 2*N_ABC*r
-    coal_A = N_A/N_ref
-    coal_B = N_B/N_ref
+    coal_A = N_ref/N_ref
+    coal_B = N_ref/N_ref
     coal_AB = N_AB/N_ref
-    coal_C = N_C/N_ref
+    coal_C = N_ref/N_ref
     coal_ABC = N_ABC/N_ref
     mu_A = mu
     mu_B = mu
@@ -76,30 +76,29 @@ def trans_emiss_calc(t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu, 
     return a, b, pi, hidden_names, observed_names
 
 def optimization_wrapper(arg_lst, n_int_AB, n_int_ABC, V, info):
-    t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu = arg_lst
+    t_1, t_2, t_upper, N_AB, N_ABC, r, mu = arg_lst
     a, b, pi, hidden_names, observed_names = trans_emiss_calc(
-        t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC
+        t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC
     )
     loglik = forward_loglik(a, b, pi, V)
-    write_list([info['Nfeval'], t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu, loglik])
+    write_list([info['Nfeval'], t_1, t_2, t_upper, N_AB, N_ABC, r, mu, loglik])
     print(
-        '{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}   {5: 3.6f}   {6: 3.6f}   {7: 3.6f}   {8: 3.6f}   {9: 3.6f}   {10: 3.6f}   {11: 3.6f}   {12: 3.6f}'.format(
+        '{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}   {5: 3.6f}   {6: 3.6f}   {7: 3.6f}   {8: 3.6f}'.format(
             info['Nfeval'], 
-            arg_lst[0], arg_lst[1], arg_lst[2], arg_lst[3], arg_lst[4], 
-            arg_lst[5], arg_lst[6], arg_lst[7], arg_lst[8], arg_lst[9], arg_lst[10],
-            loglik
+            arg_lst[0], arg_lst[1], arg_lst[2], arg_lst[3], 
+            arg_lst[4], arg_lst[5], arg_lst[6], loglik
         )
     )
     info['Nfeval'] += 1
     return -loglik
 
-def optimizer(t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC, V):
-    init_params = np.array([t_1, t_2, t_upper, N_A, N_B, N_C, N_D, N_AB, N_ABC, r, mu])
+def optimizer(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC, V):
+    init_params = np.array([t_1, t_2, t_upper, N_AB, N_ABC, r, mu])
     b_t = (0.01, 10)
     b_N = (1, 500000)
     b_r = (0.000000001, 0.00001)
     b_mu = (0.0001, 0.1)
-    bnds = (b_t, b_t, b_t, b_N, b_N, b_N, b_N, b_N, b_N, b_r, b_mu)
+    bnds = (b_t, b_t, b_t, b_N, b_N, b_r, b_mu)
     res = minimize(
         optimization_wrapper, 
         x0 = init_params,
