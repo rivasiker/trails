b2a5237f9f3df4eb617cbe49ea8c09a8dfeb89b4
rivasiker <ikerrivas96@gmail.com>
2022-09-16 16:35:54 -0400

    Change parameterization

--- src/ilsmc/optimizer.py
+++ src/ilsmc/optimizer.py
@@ -14,8 +14,8 @@ def forward_loglik(a, b, pi, V):
             alpha[t, j] = np.log(np.exp(alpha[t - 1]-x).dot(a[:, j]) * b[j, V[t]])+x
     return np.log(np.exp(alpha[len(V)-1]-x).sum())+x
 
-def write_list(lst):
-    with open('results.csv', 'a') as f:
+def write_list(lst, res_name):
+    with open('{}.csv'.format(res_name), 'a') as f:
         for i in range(len(lst)):
             f.write(str(lst[i]))
             if i != (len(lst)-1):
@@ -23,28 +23,34 @@ def write_list(lst):
         f.write('\n')
 
 def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC):
+    # Reference Ne (for normalization)
     N_ref = N_AB
-    t_A = t_1
-    t_B = t_1
-    t_AB = t_2
-    t_C = t_1+t_2
+    # Speciation times (in coalescent units, i.e. number of generations / N_ref)
+    t_A = t_1/N_ref
+    t_B = t_1/N_ref
+    t_AB = t_2/N_ref
+    t_C = (t_1+t_2)/N_ref
+    t_upper = t_upper/N_ref
     t_peak = 2*(N_ABC/N_ref)
-    rho_A = 2*N_AB*r
-    rho_B = 2*N_AB*r
-    rho_AB = 2*N_AB*r
-    rho_C = 2*N_AB*r
-    rho_ABC = 2*N_ABC*r
+    # Recombination rates (r = rec. rate per site per generation)
+    rho_A = 2*N_ref*r
+    rho_B = 2*N_ref*r
+    rho_AB = 2*N_ref*r
+    rho_C = 2*N_ref*r
+    rho_ABC = 2*N_ref*r
+    # Coalescent rates
     coal_A = N_ref/N_ref
     coal_B = N_ref/N_ref
     coal_AB = N_AB/N_ref
     coal_C = N_ref/N_ref
     coal_ABC = N_ABC/N_ref
-    mu_A = mu
-    mu_B = mu
-    mu_C = mu
-    mu_D = mu
-    mu_AB = mu
-    mu_ABC = mu
+    # Mutation rates (mu = mut. rate per site per generation)
+    mu_A = 2*N_ref*mu
+    mu_B = 2*N_ref*mu
+    mu_C = 2*N_ref*mu
+    mu_D = 2*N_ref*mu
+    mu_AB = 2*N_ref*mu
+    mu_ABC = 2*N_ref*mu
     
     tr = get_joint_prob_mat(
         t_A,    t_B,    t_AB,    t_C, 
@@ -54,7 +60,7 @@ def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC)
     tr = pd.DataFrame(tr, columns=['From', 'To', 'Prob']).pivot(index = ['From'], columns = ['To'], values = ['Prob'])
     tr.columns = tr.columns.droplevel()
     hidden_names = list(tr.columns)
-    hidden_names = dict(zip(hidden_names, range(len(hidden_names))))
+    hidden_names = dict(zip(range(len(hidden_names)), hidden_names))
     arr = np.array(tr).astype(np.float64)
     pi = arr.sum(axis=1)
     a = arr/pi[:,None]
@@ -70,42 +76,44 @@ def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC)
     em = em.sort_values(["hidden_state"])
     em = em.iloc[: , 1:]
     observed_names = list(em.columns)
-    observed_names = dict(zip(observed_names, range(len(observed_names))))
+    observed_names = dict(zip(range(len(observed_names)), observed_names))
     b = np.array(em)
     
     return a, b, pi, hidden_names, observed_names
 
-def optimization_wrapper(arg_lst, n_int_AB, n_int_ABC, V, info):
+def optimization_wrapper(arg_lst, n_int_AB, n_int_ABC, V, res_name, verbose, info):
     t_1, t_2, t_upper, N_AB, N_ABC, r, mu = arg_lst
     a, b, pi, hidden_names, observed_names = trans_emiss_calc(
         t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC
     )
     loglik = forward_loglik(a, b, pi, V)
-    write_list([info['Nfeval'], t_1, t_2, t_upper, N_AB, N_ABC, r, mu, loglik])
-    print(
-        '{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}   {5: 3.6f}   {6: 3.6f}   {7: 3.6f}   {8: 3.6f}'.format(
-            info['Nfeval'], 
-            arg_lst[0], arg_lst[1], arg_lst[2], arg_lst[3], 
-            arg_lst[4], arg_lst[5], arg_lst[6], loglik
+    write_list([info['Nfeval'], t_1, t_2, t_upper, N_AB, N_ABC, r, mu, loglik], res_name)
+    if verbose:
+        print(
+            '{0:4d}   {1: .5e}   {2: .5e}   {3: .5e}   {4: .5e}   {5: .5e}   {6: .5e}   {7: .5e}   {8: 3.6f}'.format(
+                info['Nfeval'], 
+                arg_lst[0], arg_lst[1], arg_lst[2], arg_lst[3], 
+                arg_lst[4], arg_lst[5], arg_lst[6], loglik
+            )
         )
-    )
     info['Nfeval'] += 1
     return -loglik
 
-def optimizer(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC, V):
+def optimizer(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC, V, res_name, verbose = False):
     init_params = np.array([t_1, t_2, t_upper, N_AB, N_ABC, r, mu])
-    b_t = (0.01, 10)
-    b_N = (1, 500000)
-    b_r = (0.000000001, 0.00001)
-    b_mu = (0.0001, 0.1)
+    b_t = (1e4, 2e6)
+    b_N = (1000, 100000)
+    b_r = (1e-9, 1e-7)
+    b_mu = (1e-10, 1e-8)
     bnds = (b_t, b_t, b_t, b_N, b_N, b_r, b_mu)
     res = minimize(
         optimization_wrapper, 
         x0 = init_params,
-        args = (n_int_AB, n_int_ABC, V, {'Nfeval':0}),
+        args = (n_int_AB, n_int_ABC, V, res_name, verbose, {'Nfeval':0}),
         method = 'Nelder-Mead',
         bounds = bnds, 
         options = {
+            'maxiter': 3000,
             'disp': True
         }
     )
