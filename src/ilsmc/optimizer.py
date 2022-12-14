3102dcaac91634dbab3b31ab4ab5b1982ee6a62f
Iker Rivas-González <54534843+rivasiker@users.noreply.github.com>
2022-12-14 13:42:31 -0500

    Update optimizer.py

--- src/ilsmc/optimizer.py
+++ src/ilsmc/optimizer.py
@@ -83,8 +83,8 @@ def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC)
     t_B = t_1/N_ref
     t_AB = t_2/N_ref
     t_C = (t_1+t_2)/N_ref
-    t_upper = t_upper/N_ref
-    t_peak = 2
+    t_upper = t_upper/N_ABC
+    t_peak = 2*(N_ABC/N_ref)
     # Recombination rates (r = rec. rate per site per generation)
     rho_A = 2*N_ref*r
     rho_B = 2*N_ref*r
@@ -104,7 +104,7 @@ def trans_emiss_calc(t_1, t_2, t_upper, N_AB, N_ABC, r, mu, n_int_AB, n_int_ABC)
     mu_C = 2*N_ref*mu_
     mu_D = 2*N_ref*mu_
     mu_AB = 2*N_ref*mu_
-    mu_ABC = 2*N_ABC*mu_
+    mu_ABC = 2*N_ref*mu_
     
     tr = get_joint_prob_mat(
         t_A,    t_B,    t_AB,    t_C, 
