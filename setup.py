89b85e54860842d8f3b3345994e753b1ccc749e9
Iker Rivas-González <54534843+rivasiker@users.noreply.github.com>
2023-01-06 11:45:07 -0500

    Change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="trails-rivasiker",
-    version="0.0.6",
+    version="0.0.1",
     author="Iker Rivas-González",
     author_email="irg@birc.au.dk",
     description="Tree reconstruction of the ancestry using incomplete lineage sorting",
