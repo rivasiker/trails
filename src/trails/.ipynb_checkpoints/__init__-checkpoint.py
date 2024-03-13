d5f72783689e8551855d423a232e9f2f517ad202
Iker Rivas-González <54534843+rivasiker@users.noreply.github.com>
2024-03-13 16:29:03 +0100

    Delete src/trails/.ipynb_checkpoints directory

--- src/trails/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import sys
-import numpy as np
-import pandas as pd
-from scipy.linalg import expm
-from scipy.stats import truncexpon
-from scipy.stats import expon
-from scipy.special import comb
-import ast
-import multiprocessing as mp
